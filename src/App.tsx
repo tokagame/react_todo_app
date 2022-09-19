860afa546eb26c01a2aa59c81f5641b075871480
tokapp <tokagame64@gmail.com>
2022-09-19 21:46:29 +0900

    add todo app files.

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+export const App = () => {
+  const [text, setText] = useState('');
+  return (
+    <div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <input
+          type="submit"
+          value="追加"
+          onSubmit={(e) => e.preventDefault()
+        }/>
+      </form>
+    </div>
+  )
+}
