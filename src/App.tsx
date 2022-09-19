300d56e89fcb84b0a76a76e615cd62ea29e75ccf
tokapp <tokagame64@gmail.com>
2022-09-19 23:21:19 +0900

    TODO 追加機能を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,48 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
+
+type Todo = {
+  value: string;
+}
 
 export const App = () => {
   const [text, setText] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  }
+
+  // toods ステートを更新
+  const handleOnSubmit = () => {
+    if (!text) {
+      // テキスト欄への入力が無い
+      return ;
+    }
+
+    const newTodo: Todo = {
+      value: text,
+    }
+
+    // スプレッド構文を用いて todos ステートのコピーへ newTodo を追加する
+    setTodos([newTodo, ...todos]);
+    // フォーム欄をクリア
+    setText('');
+  }
+
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form onSubmit={(e) => handleOnSubmit()}>
         <input
           type="text"
           value={text}
-          onChange={(e) => setText(e.target.value)}
+          onChange={(e) => handleOnChange(e)}
         />
         <input
           type="submit"
           value="追加"
-          onSubmit={(e) => e.preventDefault()
-        }/>
+          onSubmit={handleOnSubmit}
+        />
       </form>
     </div>
   )
