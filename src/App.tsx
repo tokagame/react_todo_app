8db8099a77401ba6f6442f700938daf61125051c
tokapp <tokagame64@gmail.com>
2022-09-21 23:44:13 +0900

    タスクの削除機能を追加

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ type Todo = {
   value: string;
   readonly id: number;
   checked: boolean;
+  removed: boolean;
 }
 
 export const App = () => {
@@ -29,6 +30,7 @@ export const App = () => {
       value: text,
       id: new Date().getTime(),
       checked: false,
+      removed: false,
     }
 
     // スプレッド構文を用いて todos ステートのコピーへ newTodo を追加する
@@ -65,6 +67,20 @@ export const App = () => {
     setTodos(newTodos)
   }
 
+  const handleOnRemove = (id: number, removed: boolean) => {
+    const deepCopy = todos.map((todo) => ({ ...todo }));
+
+    const newTodos = deepCopy.map((todo) => {
+      if (todo.id === id) {
+        todo.removed = !removed;
+      }
+
+      return todo;
+    })
+
+    setTodos(newTodos);
+  }
+
   return (
     <div>
       <form onSubmit={(e) => handleOnSubmit(e)}>
@@ -91,9 +107,13 @@ export const App = () => {
               />
               <input
                 type="text"
+                disabled={todo.removed}
                 value={todo.value}
                 onChange={(e) => handleOnEdit(todo.id, e.target.value)}
               />
+              <button onClick={() => handleOnRemove(todo.id, todo.removed)}>
+                {todo.removed ? '復元' : '削除'}
+              </button>
             </li>
           )
         })}
